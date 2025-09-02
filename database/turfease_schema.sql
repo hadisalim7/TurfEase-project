1ddbf6e6bff922d9df535cc921d2b4e901aeed01
alokkrishnah <alokkrishnah2004@gmail.com>
2025-09-02 19:39:42 +0530

    updated database

--- database/turfease_schema.sql
+++ database/turfease_schema.sql
@@ -113,9 +113,10 @@ CREATE TABLE `users` (
   `email` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
   `phone` varchar(15) DEFAULT NULL,
+  `role` enum('user','admin') DEFAULT 'user',
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -124,6 +125,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'HadiSalim','sosocan124@gmail.com','mashaallah','7592021946','user'),(2,'Admin One','admin1@turfease.com','admin123','9999999991','admin'),(3,'Admin Two','admin2@turfease.com','admin456','9999999992','admin');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -136,4 +138,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-26 16:50:02
+-- Dump completed on 2025-09-02 19:39:00
