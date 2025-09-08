6d28bf2993f2c7d8aeb05de95f222664fa22c7cc
Maazin000 <maazin.ismail@gmail.com>
2025-09-08 17:23:39 +0530

    testing

--- src/com/turfease/app/Main.java
+++ src/com/turfease/app/Main.java
@@ -2,6 +2,6 @@ package com.turfease.app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TurfEase project is readydone!");
+        System.out.println("TurfEase project is ready done!");
     }
 }
