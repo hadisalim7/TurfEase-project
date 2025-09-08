8f476c24f35ab2c5922f4fd053cd123cd0e08332
hadisalim7 <sosocan124@gmail.com>
2025-09-08 18:27:00 +0530

    completed phase 3

--- src/com/turfease/app/Main.java
+++ src/com/turfease/app/Main.java
@@ -2,6 +2,6 @@ package com.turfease.app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TurfEase project is  ready done!");
+        System.out.println("completed phase 3");
     }
 }
