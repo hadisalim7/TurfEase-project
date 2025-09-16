1259f2a351aa4143364ffc7a367d57acfc3c78bf
alokkrishnah <alokkrishnah@gmail.com>
2025-09-16 07:41:23 +0530

    updated

--- src/com/turfease/app/Main.java
+++ src/com/turfease/app/Main.java
@@ -4,7 +4,7 @@ import com.turfease.frontend.SplashScreen;
 
 public class Main {
     public static void main(String[] args) {
-        // Start app with SplashScreen
+        // Start app with SplashScreeen testing
         new SplashScreen();
     }
 }
